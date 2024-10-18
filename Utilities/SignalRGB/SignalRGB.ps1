94021d384d8ec0d1fe544e2b4bf72ef6ef21c3c0
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-18 09:34:50 -0400

    Update SignalRGB.ps1

--- Utilities/SignalRGB/SignalRGB.ps1
+++ Utilities/SignalRGB/SignalRGB.ps1
@@ -1,11 +1,29 @@
-$SignalRGBURL1 = ""
-$SignalRGBURL2 = ""
-$SignalRGBURL3 = ""
-$SignalRGBURL4 = ""
-$SignalRGBURL5 = ""
-$SignalRGBURL6 = ""
-$SignalRGBURL7 = ""
-$SignalRGBURL8 = ""
-$SignalRGBURL9 = ""
+$SignalRGBURL1 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.001"
+$SignalRGBURL2 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.002"
+$SignalRGBURL3 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.003"
+$SignalRGBURL4 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.004"
+$SignalRGBURL5 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.005"
+$SignalRGBURL6 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.006"
+$SignalRGBURL7 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.007"
+$SignalRGBURL8 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.008"
+$SignalRGBURL9 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.009"
+
+$SignalREGBDestination = "C:\downloads"
+
+$OSD = (Get-Module -ListAvailable -Name OSD)
+
+If($OSD -eq $false){
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name OSD -Force
+
+Save-WebFile 
 
 
+}
+
+ElseIf($OSD -eq $true){
+Import-Module -Name OSD -Force 
+Save-WebFile 
+
+
+}
