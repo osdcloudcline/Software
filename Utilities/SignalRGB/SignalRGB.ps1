0857e65a048e4dea25f3859e43a996ac1ab62361
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-18 09:24:55 -0400

    Update SignalRGB.ps1

--- Utilities/SignalRGB/SignalRGB.ps1
+++ Utilities/SignalRGB/SignalRGB.ps1
@@ -1,1 +1,11 @@
+$SignalRGBURL1 = ""
+$SignalRGBURL2 = ""
+$SignalRGBURL3 = ""
+$SignalRGBURL4 = ""
+$SignalRGBURL5 = ""
+$SignalRGBURL6 = ""
+$SignalRGBURL7 = ""
+$SignalRGBURL8 = ""
+$SignalRGBURL9 = ""
+
 
