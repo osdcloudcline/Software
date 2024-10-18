ae02aa9e63fcd81bd693471414d721052dadd284
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-10-18 12:44:02 -0400

    Update SignalRGB.ps1

--- Utilities/SignalRGB/SignalRGB.ps1
+++ Utilities/SignalRGB/SignalRGB.ps1
@@ -1,1 +1,46 @@
+$SignalRGBURL1 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.001"
+$SignalRGBURL2 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.002"
+$SignalRGBURL3 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.003"
+$SignalRGBURL4 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.004"
+$SignalRGBURL5 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.005"
+$SignalRGBURL6 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.006"
+$SignalRGBURL7 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.007"
+$SignalRGBURL8 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.008"
+$SignalRGBURL9 = "https://github.com/osdcloudcline/Software/raw/refs/heads/main/Utilities/SignalRGB/ver.%202.4.17/Install_SignalRgb.zip.009"
 
+$SignalRGBDestination = "C:\downloads"
+
+$OSD = (Get-Module -ListAvailable -Name OSD)
+
+If($OSD -eq $false){
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck
+Import-Module -Name OSD -Force
+
+Save-WebFile -SourceUrl $SignalRGBURL1 -DestinationDirectory $SignalRGBDestination
+Save-WebFile -SourceUrl $SignalRGBURL2 -DestinationDirectory $SignalRGBDestination
+Save-WebFile -SourceUrl $SignalRGBURL3 -DestinationDirectory $SignalRGBDestination
+Save-WebFile -SourceUrl $SignalRGBURL4 -DestinationDirectory $SignalRGBDestination
+Save-WebFile -SourceUrl $SignalRGBURL5 -DestinationDirectory $SignalRGBDestination
+Save-WebFile -SourceUrl $SignalRGBURL6 -DestinationDirectory $SignalRGBDestination
+Save-WebFile -SourceUrl $SignalRGBURL7 -DestinationDirectory $SignalRGBDestination
+Save-WebFile -SourceUrl $SignalRGBURL8 -DestinationDirectory $SignalRGBDestination
+Save-WebFile -SourceUrl $SignalRGBURL9 -DestinationDirectory $SignalRGBDestination
+
+Expand-Archive -Path "$SignalRGBDestination\Install_SignalRgb.zip" -DestinationPath $SignalRGBDestination
+}
+
+ElseIf($OSD -eq $true){
+Import-Module -Name OSD -Force 
+
+Save-WebFile -SourceUrl $SignalRGBURL1 -DestinationDirectory $SignalRGBDestination
+Save-WebFile -SourceUrl $SignalRGBURL2 -DestinationDirectory $SignalRGBDestination
+Save-WebFile -SourceUrl $SignalRGBURL3 -DestinationDirectory $SignalRGBDestination
+Save-WebFile -SourceUrl $SignalRGBURL4 -DestinationDirectory $SignalRGBDestination
+Save-WebFile -SourceUrl $SignalRGBURL5 -DestinationDirectory $SignalRGBDestination
+Save-WebFile -SourceUrl $SignalRGBURL6 -DestinationDirectory $SignalRGBDestination
+Save-WebFile -SourceUrl $SignalRGBURL7 -DestinationDirectory $SignalRGBDestination
+Save-WebFile -SourceUrl $SignalRGBURL8 -DestinationDirectory $SignalRGBDestination
+Save-WebFile -SourceUrl $SignalRGBURL9 -DestinationDirectory $SignalRGBDestination
+
+Expand-Archive -Path "$SignalRGBDestination\Install_SignalRgb.zip" -DestinationPath $SignalRGBDestination
+}
