1ad03475d4952ef4acc98e6e0cf6ee6ffd881c9d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-07-31 09:49:42 -0400

    Create WSA.ps1

---
+++ Utilities/MSIX Bundles/WSA/WSA.ps1
@@ -0,0 +1,122 @@
+$WSADLURL1 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.001"
+$WSADLURL2 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.002"
+$WSADLURL3 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.003"
+$WSADLURL4 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.004"
+$WSADLURL5 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.005"
+$WSADLURL6 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.006"
+$WSADLURL7 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.007"
+$WSADLURL8 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.008"
+$WSADLURL9 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.009"
+$WSADLURL10 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.010"
+$WSADLURL11 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.011"
+$WSADLURL12 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.012"
+$WSADLURL13 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.013"
+$WSADLURL14 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.014"
+$WSADLURL15 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.015"
+$WSADLURL16 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.016"
+$WSADLURL17 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.017"
+$WSADLURL18 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.018"
+$WSADLURL19 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.019"
+$WSADLURL20 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.020"
+$WSADLURL21 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.021"
+$WSADLURL22 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.022"
+$WSADLURL23 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.023"
+$WSADLURL24 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.024"
+$WSADLURL25 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.025"
+$WSADLURL26 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.026"
+$WSADLURL27 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.027"
+$WSADLURL28 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.028"
+$WSADLURL29 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.029"
+$WSADLURL30 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.030"
+$WSADLURL31 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.031"
+$WSADLURL32 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.032"
+$WSADLURL33 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.033"
+$WSADLURL34 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.034"
+$WSADLURL35 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.035"
+$WSADLURL36 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.036"
+$WSADLURL37 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.037"
+$WSADLURL38 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.038"
+$WSADLURL39 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.039"
+$WSADLURL40 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.040"
+$WSADLURL41 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.041"
+$WSADLURL42 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.042"
+$WSADLURL43 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.043"
+$WSADLURL44 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.044"
+$WSADLURL45 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.045"
+$WSADLURL46 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.046"
+$WSADLURL47 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.047"
+$WSADLURL48 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.048"
+$WSADLURL49 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.049"
+$WSADLURL50 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.050"
+$WSADLURL51 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.051"
+$WSADLURL52 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.052"
+$WSADLURL53 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.053"
+$WSADLURL54 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.054"
+$WSADLURL55 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.055"
+$WSADLURL56 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.056"
+$WSADLURL57 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.057"
+$WSADLURL58 = "https://github.com/osdcloudcline/Software/blob/main/Utilities/MSIX%20Bundles/WSA/MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.zip.058"
+
+$destination = "C:\downloads\OS\MSIX\WSA"
+$WSAfile = "C:\downloads\OS\MSIX\WSA\MicrosoftCorporationII.WindowsSubsystemForAndroid_2305.40000.4.0_neutral_~_8wekyb3d8bbwe.Msixbundle"
+
+Save-WebFile --SourceUrl $WSADLURL1 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL2 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL3 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL4 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL5 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL6 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL7 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL8 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL9 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL10 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL11 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL12 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL13 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL14 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL15 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL16 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL17 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL18 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL19 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL20 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL21 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL22 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL23 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL24 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL25 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL26 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL27 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL28 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL29 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL30 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL31 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL32 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL33 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL34 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL35 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL36 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL37 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL38 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL39 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL40 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL41 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL42 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL43 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL44 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL45 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL46 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL47 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL48 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL49 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL50 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL51 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL52 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL53 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL54 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL55 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL56 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL57 -DestinationDirectory $destination
+Save-WebFile --SourceUrl $WSADLURL58 -DestinationDirectory $destination
+
+Add-AppPackage -Path $WSAfile -DisableDevelopmentMode -ForceApplicationShutdown
